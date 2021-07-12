7c6a03eb32ecaca00cf4937e82c9ba26ab61053c
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-12 10:37:12 +0900

    this 4 ways , background ch

---
+++ OopFp/js/this4Ways.js
@@ -0,0 +1,46 @@
+//new bindingthis
+
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+
+const person1 = new Person('Xavier','55');
+
+console.log(person1)
+
+//implicit binding
+const person = {
+    name : pserson = {
+        name:'Karen',
+        age:40,
+        hi(){
+            console.log(`hi ${this.name}`)
+        }
+    }
+}
+
+//explicit bingding
+const person3 ={
+    name: 'Karen',
+    age:40,
+    hi:function(){
+        console.log(`hi ${this.setTimeout}`)
+    }.bind(window)
+}
+
+person3.hi();
+
+//arrow function
+const person4 = {
+    name: 'karen',
+    age:40,
+    hi:function(){
+        var inner = ()=>{
+            console.log(`hi ${this.name}`)
+        }
+        return inner()
+    }
+}
+
+person4.hi();
